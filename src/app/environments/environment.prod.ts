299da8000c341cffbc0556850d4d2756587114f3
Sandeep Mali <ssmali1505@gmail.com>
2025-04-03 20:19:15 +0530

    initial code added

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://thingsrms.com/api' // Change this for production
+  };
+  
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://thingsrms.com/api' // Change this based on your backend
+  };
+  
