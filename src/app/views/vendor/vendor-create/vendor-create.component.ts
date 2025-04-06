9b61aa152649c18e660cd8fa7dd398fae99ced3d
Sandeep Mali <ssmali1505@gmail.com>
2025-04-06 14:57:15 +0530

    dummy screen added

--- src/app/views/vendor/vendor-create/vendor-create.component.ts
+++ src/app/views/vendor/vendor-create/vendor-create.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormFeedbackComponent, RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, FormControlDirective, FormDirective, FormLabelDirective, FormSelectDirective, FormCheckComponent, FormCheckInputDirective, FormCheckLabelDirective, ButtonDirective, ColDirective, InputGroupComponent, InputGroupTextDirective } from '@coreui/angular';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vendor-create',
-  imports: [],
+  imports: [FormFeedbackComponent, RowComponent, ColComponent, TextColorDirective, ReactiveFormsModule,CardComponent, CardHeaderComponent, CardBodyComponent, FormControlDirective, FormsModule, FormDirective, FormLabelDirective, FormSelectDirective, FormCheckComponent, FormCheckInputDirective, FormCheckLabelDirective, ButtonDirective, ColDirective, InputGroupComponent, InputGroupTextDirective],
   templateUrl: './vendor-create.component.html',
-  styleUrl: './vendor-create.component.scss'
+  styleUrl: './vendor-create.component.scss',
 })
 export class VendorCreateComponent {
 
--- src/app/views/vendor/vendor-list/vendor-list.component.ts
+++ src/app/views/vendor/vendor-list/vendor-list.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TableDirective, TableColorDirective, TableActiveDirective, BorderDirective, AlignDirective, FormFeedbackComponent, RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, FormControlDirective, FormDirective, FormLabelDirective, FormSelectDirective, FormCheckComponent, FormCheckInputDirective, FormCheckLabelDirective, ButtonDirective, ColDirective, InputGroupComponent, InputGroupTextDirective } from '@coreui/angular';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-vendor-list',
-  imports: [],
+  imports: [TableDirective, TableColorDirective, TableActiveDirective, BorderDirective, AlignDirective, FormFeedbackComponent, RowComponent, ColComponent, TextColorDirective, ReactiveFormsModule,CardComponent, CardHeaderComponent, CardBodyComponent, FormControlDirective, FormsModule, FormDirective, FormLabelDirective, FormSelectDirective, FormCheckComponent, FormCheckInputDirective, FormCheckLabelDirective, ButtonDirective, ColDirective, InputGroupComponent, InputGroupTextDirective],
   templateUrl: './vendor-list.component.html',
   styleUrl: './vendor-list.component.scss'
 })
