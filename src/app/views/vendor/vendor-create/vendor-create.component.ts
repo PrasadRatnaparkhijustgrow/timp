d4260c5ba967bca11109be161c61cf9659befb85
Sandeep Mali <ssmali1505@gmail.com>
2025-04-06 16:13:14 +0530

    integrated with data

--- src/app/views/vendor/vendor-create/vendor-create.component.ts
+++ src/app/views/vendor/vendor-create/vendor-create.component.ts
@@ -10,4 +10,31 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 })
 export class VendorCreateComponent {
 
+  vendorForm = {
+    vendorName: 'John Doe',
+    companyName: 'Doe Enterprises',
+    email: 'johndoe@example.com',
+    phone: '1234567890',
+    address: '123 Main Street',
+    city: 'Metropolis',
+    state: 'StateName',
+    district: 'DistrictName',
+    zip: '123456',
+    gst: 'GST12345',
+    pan: 'PAN12345',
+    aadhar: 'ADFAFAFAFAF',
+    accountHolder: 'John Doe',
+    bankName: 'Bank of Example',
+    branchName: 'Main Branch',
+    ifsc: 'IFSC1234',
+    acnumber: '1234567890123456'
+
+  };
+
+  constructor() {}
+
+  onSubmit() {
+    console.log('Vendor Created:', this.vendorForm);
+    // Here you can add your logic to handle the form submission, like sending the data to a server.
+  }
 }
