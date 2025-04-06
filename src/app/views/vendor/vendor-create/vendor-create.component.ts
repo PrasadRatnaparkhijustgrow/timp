0d77dda2f575775353ea43c90ab88e36c4e3c6b9
Sandeep Mali <ssmali1505@gmail.com>
2025-04-06 15:47:46 +0530

    up updated

--- src/app/views/vendor/vendor-create/vendor-create.component.ts
+++ src/app/views/vendor/vendor-create/vendor-create.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { FormFeedbackComponent, RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, FormControlDirective, FormDirective, FormLabelDirective, FormSelectDirective, FormCheckComponent, FormCheckInputDirective, FormCheckLabelDirective, ButtonDirective, ColDirective, InputGroupComponent, InputGroupTextDirective } from '@coreui/angular';
+import { RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, FormControlDirective, FormDirective, FormLabelDirective, FormSelectDirective, ButtonDirective} from '@coreui/angular';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vendor-create',
-  imports: [FormFeedbackComponent, RowComponent, ColComponent, TextColorDirective, ReactiveFormsModule,CardComponent, CardHeaderComponent, CardBodyComponent, FormControlDirective, FormsModule, FormDirective, FormLabelDirective, FormSelectDirective, FormCheckComponent, FormCheckInputDirective, FormCheckLabelDirective, ButtonDirective, ColDirective, InputGroupComponent, InputGroupTextDirective],
+  imports: [RowComponent, ColComponent, TextColorDirective, ReactiveFormsModule,CardComponent, CardHeaderComponent, CardBodyComponent, FormControlDirective, FormsModule, FormDirective, FormLabelDirective, FormSelectDirective, ButtonDirective],
   templateUrl: './vendor-create.component.html',
   styleUrl: './vendor-create.component.scss',
 })
